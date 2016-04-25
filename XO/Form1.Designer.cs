3e6b52dbc976d7669a71f422c17a8bb8fc5002b5
Darkhan <darkhan97cool@gmail.com>
2016-04-26 00:30:20 +0600

    XO

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(91, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 65);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(179, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 65);
+            this.label3.TabIndex = 2;
+            this.label3.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(4, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 76);
+            this.label4.TabIndex = 3;
+            this.label4.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(91, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 76);
+            this.label5.TabIndex = 7;
+            this.label5.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Location = new System.Drawing.Point(182, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 76);
+            this.label6.TabIndex = 6;
+            this.label6.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(7, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 70);
+            this.label7.TabIndex = 5;
+            this.label7.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(97, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 70);
+            this.label8.TabIndex = 4;
+            this.label8.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label9.Location = new System.Drawing.Point(185, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 70);
+            this.label9.TabIndex = 8;
+            this.label9.Click += new System.EventHandler(this.xo_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label10.Location = new System.Drawing.Point(287, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(123, 151);
+            this.label10.TabIndex = 9;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label11.Location = new System.Drawing.Point(287, 193);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(123, 60);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Tagi oinaisindarma";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 262);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int cnt;
+        private void xo_Click(object sender, EventArgs e)
+        {
+            Label item = (Label)sender;
+            //item.Text = "X";
+            item.Enabled = false;
+            item.Font = new Font("Arial", 35);
+            //item.BackColor = Color.BlueViolet;
+            cnt++;
+            if (cnt % 2==0)
+            {
+                item.Text = "X";
+                item.BackColor = Color.BlueViolet;
+            }
+            else if (cnt % 2 == 1)
+            {
+                item.Text = "O";
+                item.BackColor = Color.DeepPink;
+            }
+            if((label1.Text=="O" && label2.Text == "O" && label3.Text == "O") || 
+                (label4.Text == "O" && label5.Text == "O" && label6.Text == "O") || (label7.Text == "O" && label8.Text == "O" && label9.Text == "O") || (label1.Text == "O" && label5.Text == "O" && label9.Text == "O") || (label3.Text == "O" && label5.Text == "O" && label7.Text == "O") || (label1.Text == "O" && label4.Text == "O" && label7.Text == "O") || (label2.Text == "O" && label5.Text == "O" && label8.Text == "O") || (label3.Text == "O" && label6.Text == "O" && label9.Text == "O"))
+            {
+                label10.Font = new Font("Arial", 25);
+                label10.Text = "O WIN!";
+                label2.Enabled = false;
+                label3.Enabled = false;
+                label4.Enabled = false;
+                label5.Enabled = false;
+                label6.Enabled = false;
+                label7.Enabled = false;
+                label8.Enabled = false;
+                label9.Enabled = false;
+                label1.Enabled = false;
+            }
+            if ((label1.Text == "X" && label2.Text == "X" && label3.Text == "X") || (label4.Text == "X" && label5.Text == "X" && label6.Text == "X") || (label7.Text == "X" && label8.Text == "X" && label9.Text == "X") || (label1.Text == "X" && label5.Text == "X" && label9.Text == "X") || (label3.Text == "X" && label5.Text == "X" && label7.Text == "X") || (label1.Text == "X" && label4.Text == "X" && label7.Text == "X") || (label2.Text == "X" && label5.Text == "X" && label8.Text == "X") || (label3.Text == "X" && label6.Text == "X" && label9.Text == "X"))
+            {
+                label10.Font = new Font("Arial", 25);
+                label10.Text = "X WIN!";
+                label2.Enabled = false;
+                label3.Enabled = false;
+                label4.Enabled = false;
+                label5.Enabled = false;
+                label6.Enabled = false;
+                label7.Enabled = false;
+                label8.Enabled = false;
+                label9.Enabled = false;
+                label1.Enabled = false;
+
+            }
+
+            if (cnt==8 && (label10.Text !="X WIN!" || label10.Text != "O WIN!"))
+            {
+                label10.Text = " Тағы ойнаңдар!";
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            List < Label>llist= new List<Label>();
+            llist.Add(label1);
+            llist.Add(label2);
+            llist.Add(label3);
+            llist.Add(label4);
+            llist.Add(label5);
+            llist.Add(label6);
+            llist.Add(label7);
+            llist.Add(label8);
+            llist.Add(label9);
+            llist.Add(label10);
+            foreach(Label item in llist)
+            {
+                item.Text = "";
+                item.Enabled = true;
+                item.BackColor=Color.DarkGray;
+            }
+
+
+        }
+    }
+}
